e3e5d95538ff07f03ade1884417a93625e73a1c6
unknown <kbhandary672@gmail.com>
2022-11-30 18:45:42 +0530

    7 commit

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const userRoutes = require("./routes/users");
 const placesRoutes = require("./routes/places");
 const reviewsRoutes = require("./routes/reviews");
 const MongoStore = require("connect-mongo");
-const dbUrl = "mongodb://localhost:27017/india-tour";
+const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/india-tour";
 main().catch(err => console.log("Error", err));
 
 async function main() {
@@ -61,10 +61,10 @@ const sessionConfig = {
 	store,
 	resave: false,
 	saveUninitialized: true,
-	// proxy: true,
+	proxy: true,
 	cookie: {
 		httpOnly: true,
-		// secure: true,
+		secure: true,
 		expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
 		maxAge: 1000 * 60 * 60 * 24 * 7
 	}
@@ -104,7 +104,7 @@ const connectSrcUrls = [
 	"https://ka-f.fontawesome.com"
 ];
 const fontSrcUrls = ["https://fonts.gstatic.com", "https://ka-f.fontawesome.com"];
-const formActionSrc = ["https://formsubmit.co/kbhandary672@gmail.com"];
+const formActionSrc = ["https://formsubmit.co"];
 app.use(
 	helmet.contentSecurityPolicy({
 		directives: {
